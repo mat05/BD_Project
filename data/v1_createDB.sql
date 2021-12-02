eefcc012bb3229cab8185f5427336892d6bbfb0f
mat05 <58312757+mat05@users.noreply.github.com>
2021-12-02 22:26:16 +0100

    TODO 1.3 TODO 1.4 TODO 1.5

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,56 +1,80 @@
--- TODO 1.3 : Créer les tables manquantes et modifier celles ci-dessous
 create table LesRepresentations (
     nomSpec varchar(50) not null,
     dateRep date not null,
     promoRep decimal (4,2) not null,
     prixRep decimal (6,2) not null,
-    constraint pk_rep_noSpec_dateRep primary key (nomSpec, dateRep),
-    constraint ck_spec_prixBaseSpec check (prixRep >= 0),
-    constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1)
+    constraint PK_REP primary key (nomSpec, dateRep),
+    constraint CK_REP_PR check (prixRep >= 0),
+    constraint CK_REP_PR check (promoRep >= 0 and promoRep <=1)
 );
+
 
 create table LesSpectacle
-(   noSpec       integer       not null,
-    nomSpec      varchar(50)   not null,
+(   noSpec integer not null,
+    nomSpec varchar(50) not null,
     prixBaseSpec decimal(6, 2) not null,
-    constraint pk_rep_noSpec primary key  (nomSpec, prixBaseSpec),
-    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0)
+    constraint PK_SP primary key  (noSpec),
+    constraint CK_SP check (prixBaseSpec >= 0)
 );
+
 create table LesZones (
-noZone integer not null,
-catZone varchar (50) not null,
-constraint ck_p_noZone primary key (catZone),
-constraint ck_pl_noZone check (noZone > 0),
-);
+    noZone integer not null,
+    catZone varchar (50) not null,
+    constraint PK_ZN primary key (noZone),
+    constraint CK_ZN check (noZone > 0)
+    );
+
 create table LesCategories (
-catZone varchar (50) not null,
-tauxZone decimal (4,2) not null,
-constraint ck_pr_catZone primary key (catZone),
-constraint ck_pl_tauxZone check (tauxZone >= 0),
-constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
+    catZone varchar (50) not null,
+    tauxZone decimal (4,2) not null,
+    constraint PK_CAT primary key (catZone),
+    constraint CK_CAT_TZ check (tauxZone >= 0),
+    constraint CK_CAT_CZ check (catZone in ('orchestre', 'balcon', 'poulailler'))
 );
+
 create table LesTickets (
-noTicket integer not null,
-dateAchat date not null,
-dateRep date not null ,
-noPlace integer not null,
-nomSpec varchar(50) not null,
-type
-noSpec
-
-
+    noTicket integer not null,
+    dateAchat date not null,
+    dateRep date not null ,
+    noPlace integer not null,
+    nomSpec varchar(50) not null,
+    typeReduc decimal (6,2),
+    constraint PK_TK primary key(noTicket),
+    constraint  CK_TK_ check (noTicket > 0 ),
+    constraint CK_TK_TR check ( typeReduc >= 0 )
 
 );
+create table LesReduction (
+    type varchar(50) not null ,
+    typeReduc integer not null ,
+    tauxReduc decimal(6,2),
+    constraint PK_RD primary key (type),
+    constraint CK_TR check ( tauxReduc >= 0 ),
+    constraint CK_RD check (type in  ('sans reduction','adherent','etudiant', 'scolaire' , 'militaire', 'seniors' ))
+             );
+
 
 create table LesPlaces (
     noPlace integer,
     noRang integer,
     noZone integer not null,
-    tauxZone decimal (4,2) not null,
-    constraint pk_pl_noP_noR primary key (noZone, noPlace, noRang),
-    constraint ck_pl_noP check (noPlace > 0),
-    constraint ck_pl_noR check (noRang > 0),
+    constraint PK_LP primary key (noZone, noPlace, noRang),
+    constraint CK_PL_NP check (noPlace > 0),
+    constraint CK_PL_NR check (noRang > 0)
     );
+
+create table LesVentes (
+    noAchat   integer not null,
+    noTicket  integer not null,
+    prixTotal decimal(6, 2),
+    constraint PK_LV primary key (noAchat),
+    constraint FK_LV foreign key (noTicket) references LesTickets(noTicket),
+    constraint CK_LV check ( prixTotal >= 0 )
+);
+
 -- TODO 1.4 : Créer une vue LesRepresentations ajoutant le nombre de places disponible et d'autres possibles attributs calculés.
+
+
+
 -- TODO 1.5 : Créer une vue  avec le noDos et le montant total correspondant.
--- TODO 3.3 : Ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
+-- TODO 3.3 : Ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracl
--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -1,3 +1,9 @@
 -- TODO 1.3 : Détruire les tables manquantes et modifier celles ci-dessous
 DROP TABLE IF EXISTS LesPlaces;
 DROP TABLE IF EXISTS LesRepresentations;
+DROP TABLE IF EXISTS LesSpectacles;
+DROP TABLE IF EXISTS LesTickets;
+DROP TABLE IF EXISTS LesCategories;
+DROP TABLE IF EXISTS LesReductions;
+DROP TABLE IF EXISTS LesZones;
+DROP TABLE IF EXISTS LesVentes;
