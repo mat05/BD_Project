1415506656022da2592752c2b7d08e2612c1a8db
mat05 <58312757+mat05@users.noreply.github.com>
2021-12-02 11:07:25 +0100

    Version d'essai NULLLL

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,31 +1,56 @@
 -- TODO 1.3 : Créer les tables manquantes et modifier celles ci-dessous
 create table LesRepresentations (
-    noSpec integer not null,
     nomSpec varchar(50) not null,
     dateRep date not null,
     promoRep decimal (4,2) not null,
-    prixBaseSpec decimal (6,2) not null,
     prixRep decimal (6,2) not null,
-    constraint pk_rep_noSpec_dateRep primary key (noSpec, dateRep),
-    constraint ck_rep_noSpec check (noSpec > 0),
-    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0),
+    constraint pk_rep_noSpec_dateRep primary key (nomSpec, dateRep),
     constraint ck_spec_prixBaseSpec check (prixRep >= 0),
     constraint ck_rep_promoRep check (promoRep >= 0 and promoRep <=1)
+);
+
+create table LesSpectacle
+(   noSpec       integer       not null,
+    nomSpec      varchar(50)   not null,
+    prixBaseSpec decimal(6, 2) not null,
+    constraint pk_rep_noSpec primary key  (nomSpec, prixBaseSpec),
+    constraint ck_spec_prixBaseSpec check (prixBaseSpec >= 0)
+);
+create table LesZones (
+noZone integer not null,
+catZone varchar (50) not null,
+constraint ck_p_noZone primary key (catZone),
+constraint ck_pl_noZone check (noZone > 0),
+);
+create table LesCategories (
+catZone varchar (50) not null,
+tauxZone decimal (4,2) not null,
+constraint ck_pr_catZone primary key (catZone),
+constraint ck_pl_tauxZone check (tauxZone >= 0),
+constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
+);
+create table LesTickets (
+noTicket integer not null,
+dateAchat date not null,
+dateRep date not null ,
+noPlace integer not null,
+nomSpec varchar(50) not null,
+type
+noSpec
+
+
+
 );
 
 create table LesPlaces (
     noPlace integer,
     noRang integer,
     noZone integer not null,
-    catZone varchar (50) not null,
     tauxZone decimal (4,2) not null,
     constraint pk_pl_noP_noR primary key (noZone, noPlace, noRang),
     constraint ck_pl_noP check (noPlace > 0),
     constraint ck_pl_noR check (noRang > 0),
-    constraint ck_pl_noZone check (noZone > 0),
-    constraint ck_pl_tauxZone check (tauxZone >= 0),
-    constraint ck_pl_cat check (catZone in ('orchestre', 'balcon', 'poulailler'))
-);
+    );
 -- TODO 1.4 : Créer une vue LesRepresentations ajoutant le nombre de places disponible et d'autres possibles attributs calculés.
 -- TODO 1.5 : Créer une vue  avec le noDos et le montant total correspondant.
 -- TODO 3.3 : Ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
