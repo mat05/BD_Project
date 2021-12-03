4b9590337b645660767dc8fb41df8e17eb6c643f
mat05 <58312757+mat05@users.noreply.github.com>
2021-12-03 02:34:10 +0100

    Test Insertion_données

--- data/v1_insertDB.sql
+++ data/v1_insertDB.sql
@@ -1,4 +1,5 @@
 -- TODO 1.3 : Inventer des données dans les nouvelles tables (tout ce qui concerne les tickets). Par contre, utiliser une requête avec insert into qui transfère les données des tables de la V0 à celles de cette nouvelle version.
-INSERT INTO LesRepresentations (nomSpec, dateRep, PromoRep,prixRep)
-select nomSpec, dateRep, promoRep, prixBaseSpec
-from V0_LesRepresentations
+INSERT INTO LesRepresentations (nomSpec, dateRep, PromoRep,prixRep) SELECT nomSpec, dateRep, promoRep, prixBaseSpec
+FROM V0_LesRepresentations;
+
+/*INSERT INTO LesPlaces (noPlace, noRang, noZone) SELECT noPlace,noRang,noZone FROM v0_LesPlaces */
