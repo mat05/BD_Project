b601120bae406346050444d97f82d0d560c29175
mat05 <58312757+mat05@users.noreply.github.com>
2021-12-04 12:12:47 +0100

    Test Insertion_données

--- data/v1_insertDB.sql
+++ data/v1_insertDB.sql
@@ -1,5 +1,44 @@
 -- TODO 1.3 : Inventer des données dans les nouvelles tables (tout ce qui concerne les tickets). Par contre, utiliser une requête avec insert into qui transfère les données des tables de la V0 à celles de cette nouvelle version.
-INSERT INTO LesRepresentations (nomSpec, dateRep, PromoRep,prixRep) SELECT nomSpec, dateRep, promoRep, prixBaseSpec
-FROM V0_LesRepresentations;
 
-/*INSERT INTO LesPlaces (noPlace, noRang, noZone) SELECT noPlace,noRang,noZone FROM v0_LesPlaces */
+INSERT INTO LesPlaces (noPlace, noRang, noZone) SELECT noPlace,noRang,noZone FROM v0_LesPlaces ;
+
+INSERT INTO LesSpectacle (noSpec, nomSpec, prixBaseSpec) VALUES (1,'How to be a parisian',25);
+
+
+
+
+
+
+
+
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'20/12/2019 14:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'15/10/2019 12:00', null, 1, null,2);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'22/12/2019 18:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'24/12/2019 14:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'22/12/2019 13:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'21/12/2019 12:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'20/12/2019 11:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'21/12/2019 09:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'28/12/2019 18:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'12/12/2019 14:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'23/12/2019 16:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'13/12/2019 17:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'13/12/2019 18:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'08/12/2019 16:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'26/12/2019 13:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'07/12/2019 13:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'16/12/2019 11:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'23/12/2019 13:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'16/08/2019 12:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'06/03/2019 14:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'03/05/2019 15:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'06/06/2019 16:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'29/10/2019 14:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'10/10/2019 12:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'12/11/2019 12:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'16/12/2019 13:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'15/12/2019 11:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'23/07/2019 10:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'08/11/2019 18:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'12/11/2019 16:00', null, 1, null,1);
+INSERT INTO LesTickets(noTicket, dateAchat, dateRep, noPlace, nomSpec, typeReduc) VALUES (1,'07/10/2019 17:00', null, 1, null,1);
